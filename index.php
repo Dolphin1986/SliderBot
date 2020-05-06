24e4d0770c9952c8ecc5d0561b06cdb5f067eb0b
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-06 12:32:02 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$data = file_get_contents('php://input');
+$data = json_decode($data, true);
+
+ob_start();
+print_r($data);
+$out = ob_get_clean(); 
+file_put_contents(__DIR__ . '/message.txt', $out);
+
+
+
+if($json = json_decode(file_get_contents("php://input"), true)) {
+    print_r($json);
+    $data = $json;
+} else {
+    print_r($_POST);
+    $data = $_POST;
+}
+
+?>
